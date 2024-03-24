bcc67af3b26805efecf8b2907e9c79967afcdee0
Joshua King <joshuajking84@gmail.com>
2024-03-23 21:51:54 -0700

    MDU-017
    * utils/character_link.py: Refactor for 30sec login to each character

--- utils/character_link.py
+++ utils/character_link.py
@@ -1,58 +1,20 @@
-from time import sleep, perf_counter
+from time import sleep
 
-from logs.logging_config import logger
-
-from config.config_manager import ConfigManager
-from utils.read_json import read_json, is_package
-
-config_manager = ConfigManager()
-character_list = config_manager.get_value('config.character_list')
-app_path = config_manager.get_value('config.app_path')
-
-
-# TODO want to incorprate this into the start and ask for user input
+from core.DUCharacters import DUCharacters
+from querysets.querysets import CharacterQuerySet
 
 
 def character_link():
-    """
-
-    :return:
-    """
-
-    character_progress = read_json(config_manager.get_value('config.character_progress'))  # Get the characters dictionary
+    all_active_characters = CharacterQuerySet.get_active_characters()  # Get the characters dictionary
     # loop over all the accounts
-    for username, character_info in character_progress['characters'].items():
-        email = character_info.get('email')
-        password = character_info.get('pwd')
-        logger.info(f'Attempting login: {email}')
+    for character in all_active_characters:
+        has_gametime = DUCharacters().login(character)
 
-        character_time_start = perf_counter()
-        login_character_result = login_character(username=username, email=email, password=password)
-
-        if login_character_result['status'] == 'nogameTime':
-            is_package(has_game_time=0, username=username)
+        if not has_gametime:
             continue
+        sleep(30)
 
-        elif login_character_result['status'] == 'loggedIn':
-            logout(respawn=False)
-            character_time_stop = perf_counter()
-            logger.info(f'character_timing: {character_time_stop - character_time_start}')
-            continue
-
-        elif login_character_result['status'] == 'successfulLogin':
-            result = char_check()
-            has_package = result.get('has_package')
-
-            is_package(has_game_time=1, has_package=has_package, username=username)
-
-            sleep(config_manager.get_value('config.char_sleep_time'))
-
-            logout(respawn=False)
-
-        character_time_stop = perf_counter()
-        logger.info(f'character_link_timing: {character_time_stop - character_time_start}')
-
-        continue
+        DUCharacters().logout()
 
 
 if __name__ == "__main__":
