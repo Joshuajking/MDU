50df22921b10da169559c5e1ba61eee6fca57e4b
Joshua King <joshuajking84@gmail.com>
2024-03-27 00:44:22 -0700

    MDU-012
    * Create model dumps to json for select db models.
    
    * Refactor engine to main for thread termination and better control.

--- config/db_setup.py
+++ config/db_setup.py
@@ -11,7 +11,6 @@ from models.models import SearchArea, Character, Image
 from path_router import DirectoryPaths
 from config.config_manager import ConfigManager
 from querysets.querysets import engine
-from utils.data_preprocessor import DataPreprocessor
 
 
 class DbConfig:
@@ -133,116 +132,117 @@ class DbConfig:
 		raise ValueError(f"region_name: {region_name} was not found in")
 
 
+
 if __name__ == '__main__':
 	obj = DbConfig()
 	# obj.create_db_and_tables()
 	obj.manual_load_character()
-	# obj.load_image_entries_to_db()
+# obj.load_image_entries_to_db()
 
-	# obj.delete_image_from_db()
-	#
-	# from models.models import SearchAreaLocation
-	#
-	# area = {
-	# 	"ACTIVE_TAKEN_MISSIONS": {
-	# 		"region_name": SearchAreaLocation.ACTIVE_TAKEN_MISSIONS,
-	# 		"image_path": r"C:\Repositories\Dual Universe\Missions Dual Universe\data\search_areas\ACTIVE_TAKEN_MISSIONS.png"
-	# 	},
-	# 	"AVAILABLE_MISSIONS": {
-	# 		"region_name": SearchAreaLocation.AVAILABLE_MISSIONS,
-	# 		"image_path": r"C:\Repositories\Dual Universe\Missions Dual Universe\data\search_areas\AVAILABLE_MISSIONS.png"
-	# 	},
-	# 	"RETRIEVE_DELIVERY_STATUS": {
-	# 		"region_name": SearchAreaLocation.RETRIEVE_DELIVERY_STATUS,
-	# 		"image_path": r"C:\Repositories\Dual Universe\Missions Dual Universe\data\search_areas\unselected_deliver_package.png"
-	# 	},
-	# }
-	#
-	# obj.get_image_bbox(region_name=SearchAreaLocation.ACTIVE_TAKEN_MISSIONS,
-	#                    image_path=r"C:\Repositories\Dual Universe\MDU\data\search_areas\ACTIVE_TAKEN_MISSIONS.png")
-	#
-	# obj.get_image_bbox(region_name=SearchAreaLocation.AVAILABLE_MISSIONS,
-	#                image_path=r"C:\Users\joshu\Pictures\Screenshots\Screenshot 2024-03-02 091333.png")
+# obj.delete_image_from_db()
+#
+# from models.models import SearchAreaLocation
+#
+# area = {
+# 	"ACTIVE_TAKEN_MISSIONS": {
+# 		"region_name": SearchAreaLocation.ACTIVE_TAKEN_MISSIONS,
+# 		"image_path": r"C:\Repositories\Dual Universe\Missions Dual Universe\data\search_areas\ACTIVE_TAKEN_MISSIONS.png"
+# 	},
+# 	"AVAILABLE_MISSIONS": {
+# 		"region_name": SearchAreaLocation.AVAILABLE_MISSIONS,
+# 		"image_path": r"C:\Repositories\Dual Universe\Missions Dual Universe\data\search_areas\AVAILABLE_MISSIONS.png"
+# 	},
+# 	"RETRIEVE_DELIVERY_STATUS": {
+# 		"region_name": SearchAreaLocation.RETRIEVE_DELIVERY_STATUS,
+# 		"image_path": r"C:\Repositories\Dual Universe\Missions Dual Universe\data\search_areas\unselected_deliver_package.png"
+# 	},
+# }
+#
+# obj.get_image_bbox(region_name=SearchAreaLocation.ACTIVE_TAKEN_MISSIONS,
+#                    image_path=r"C:\Repositories\Dual Universe\MDU\data\search_areas\ACTIVE_TAKEN_MISSIONS.png")
+#
+# obj.get_image_bbox(region_name=SearchAreaLocation.AVAILABLE_MISSIONS,
+#                image_path=r"C:\Users\joshu\Pictures\Screenshots\Screenshot 2024-03-02 091333.png")
 
-	# obj.get_image_bbox(region_name=SearchAreaLocation.RETRIEVE_DELIVERY_STATUS,
-	#                    image_path=r"C:\Users\joshu\Pictures\Screenshots\Screenshot 2024-03-02 091451.png")
+# obj.get_image_bbox(region_name=SearchAreaLocation.RETRIEVE_DELIVERY_STATUS,
+#                    image_path=r"C:\Users\joshu\Pictures\Screenshots\Screenshot 2024-03-02 091451.png")
 
-	# obj.get_image_bbox(region_name=SearchAreaLocation.ABANDON_MISSION,
-	#                image_path=r"C:\Users\joshu\Pictures\Screenshots\Screenshot 2024-03-02 091601.png")
+# obj.get_image_bbox(region_name=SearchAreaLocation.ABANDON_MISSION,
+#                image_path=r"C:\Users\joshu\Pictures\Screenshots\Screenshot 2024-03-02 091601.png")
 
-	# obj.get_image_bbox(region_name=SearchAreaLocation.WARP_TARGET_DEST,
-	#                image_path=r"C:\Users\joshu\Pictures\Screenshots\Screenshot 2024-03-02 092011.png")
+# obj.get_image_bbox(region_name=SearchAreaLocation.WARP_TARGET_DEST,
+#                image_path=r"C:\Users\joshu\Pictures\Screenshots\Screenshot 2024-03-02 092011.png")
 
-	# obj.get_image_bbox(region_name=SearchAreaLocation.ORBITAL_HUD_LANDED,
-	#                    image_path=r"C:\Users\joshu\Pictures\Screenshots\Screenshot 2024-03-02 092202.png",
-	#                    confidence=0.6,
-	#                    )
-	#
-	# obj.get_image_bbox(region_name=SearchAreaLocation.SPACE_FUEL,
-	#                    image_path=r"C:\Users\joshu\Pictures\Screenshots\Screenshot 2024-03-02 093227.png")
-	#
-	# obj.get_image_bbox(region_name=SearchAreaLocation.ATMO_FUEL,
-	#                    image_path=r"C:\Users\joshu\Pictures\Screenshots\Screenshot 2024-03-02 093227.png")
+# obj.get_image_bbox(region_name=SearchAreaLocation.ORBITAL_HUD_LANDED,
+#                    image_path=r"C:\Users\joshu\Pictures\Screenshots\Screenshot 2024-03-02 092202.png",
+#                    confidence=0.6,
+#                    )
+#
+# obj.get_image_bbox(region_name=SearchAreaLocation.SPACE_FUEL,
+#                    image_path=r"C:\Users\joshu\Pictures\Screenshots\Screenshot 2024-03-02 093227.png")
+#
+# obj.get_image_bbox(region_name=SearchAreaLocation.ATMO_FUEL,
+#                    image_path=r"C:\Users\joshu\Pictures\Screenshots\Screenshot 2024-03-02 093227.png")
 
-	# region_dict = {
+# region_dict = {
 
-		# "DEST_INFO": {
-		# 	"region_name": SearchAreaLocation.DEST_INFO,
-		# 	"image_path": r"C:\Users\joshu\Pictures\Screenshots\Screenshot 2024-03-02 085354.png"
-		# },
-		# "SAFE_ZONE": {
-		# 	"region_name": SearchAreaLocation.SAFE_ZONE,
-		# 	"image_path": r"C:\Users\joshu\Pictures\Screenshots\Screenshot 2024-03-02 085458.png"
-		# },
-		# "RIDE": {
-		# 	"region_name": SearchAreaLocation.RIDE,
-		# 	"image_path": r"C:\Users\joshu\Pictures\Screenshots\Screenshot 2024-03-02 085513.png"
-		# },
-		# "DISTANCE": {
-		# 	"region_name": SearchAreaLocation.DISTANCE,
-		# 	"image_path": r"C:\Users\joshu\Pictures\Screenshots\Screenshot 2024-03-02 085507.png"
-		# },
-		# "STATUS": {
-		# 	"region_name": SearchAreaLocation.STATUS,
-		# 	"image_path": r"C:\Users\joshu\Pictures\Screenshots\Screenshot 2024-03-02 085616.png"
-		# },
-		# "ORIGIN_INFO": {
-		# 	"region_name": SearchAreaLocation.ORIGIN_INFO,
-		# 	"image_path": r"C:\Users\joshu\Pictures\Screenshots\Screenshot 2024-03-02 085344.png"
-		# },
-		# "MASS": {
-		# 	"region_name": SearchAreaLocation.MASS,
-		# 	"image_path": r"C:\Users\joshu\Pictures\Screenshots\Screenshot 2024-03-02 085436.png"
-		# },
-		# "VOLUME": {
-		# 	"region_name": SearchAreaLocation.VOLUME,
-		# 	"image_path": r"C:\Users\joshu\Pictures\Screenshots\Screenshot 2024-03-02 085447.png"
-		# },
-		# "REWARD_INFO": {
-		# 	"region_name": SearchAreaLocation.REWARD_INFO,
-		# 	"image_path": r"C:\Users\joshu\Pictures\Screenshots\Screenshot 2024-03-02 085154.png"
-		# },
-		# "TITLE_INFO": {
-		# 	"region_name": SearchAreaLocation.TITLE_INFO,
-		# 	"image_path": r"C:\Users\joshu\Pictures\Screenshots\Screenshot 2024-03-02 085603.png"
-		# },
-		# "ORIGIN_POS": {
-		# 	"region_name": SearchAreaLocation.ORIGIN_POS,
-		# 	"image_path": r"C:\Users\joshu\Pictures\Screenshots\Screenshot 2024-03-03 014408.png"
-		# },
-		# "DEST_POS": {
-		# 	"region_name": SearchAreaLocation.DEST_POS,
-		# 	"image_path": r"C:\Users\joshu\Pictures\Screenshots\Screenshot 2024-03-03 020140.png"
-		# },
-	# 	"ORBITAL_HUD_LANDED": {
-	# 		"region_name": SearchAreaLocation.ORBITAL_HUD_LANDED,
-	# 		"image_path": r"C:\Repositories\Dual Universe\Missions Dual Universe\data\search_areas\ORBITAL_HUD_LANDED.png"
-	# 	},
-	# }
-	#
-	# for key, value in region_dict.items():
-	# 	region_name = value["region_name"]
-	# 	image_path = value["image_path"]
-	#
-	# 	obj.get_image_bbox(region_name=region_name,
-	# 	                   image_path=image_path)
+# "DEST_INFO": {
+# 	"region_name": SearchAreaLocation.DEST_INFO,
+# 	"image_path": r"C:\Users\joshu\Pictures\Screenshots\Screenshot 2024-03-02 085354.png"
+# },
+# "SAFE_ZONE": {
+# 	"region_name": SearchAreaLocation.SAFE_ZONE,
+# 	"image_path": r"C:\Users\joshu\Pictures\Screenshots\Screenshot 2024-03-02 085458.png"
+# },
+# "RIDE": {
+# 	"region_name": SearchAreaLocation.RIDE,
+# 	"image_path": r"C:\Users\joshu\Pictures\Screenshots\Screenshot 2024-03-02 085513.png"
+# },
+# "DISTANCE": {
+# 	"region_name": SearchAreaLocation.DISTANCE,
+# 	"image_path": r"C:\Users\joshu\Pictures\Screenshots\Screenshot 2024-03-02 085507.png"
+# },
+# "STATUS": {
+# 	"region_name": SearchAreaLocation.STATUS,
+# 	"image_path": r"C:\Users\joshu\Pictures\Screenshots\Screenshot 2024-03-02 085616.png"
+# },
+# "ORIGIN_INFO": {
+# 	"region_name": SearchAreaLocation.ORIGIN_INFO,
+# 	"image_path": r"C:\Users\joshu\Pictures\Screenshots\Screenshot 2024-03-02 085344.png"
+# },
+# "MASS": {
+# 	"region_name": SearchAreaLocation.MASS,
+# 	"image_path": r"C:\Users\joshu\Pictures\Screenshots\Screenshot 2024-03-02 085436.png"
+# },
+# "VOLUME": {
+# 	"region_name": SearchAreaLocation.VOLUME,
+# 	"image_path": r"C:\Users\joshu\Pictures\Screenshots\Screenshot 2024-03-02 085447.png"
+# },
+# "REWARD_INFO": {
+# 	"region_name": SearchAreaLocation.REWARD_INFO,
+# 	"image_path": r"C:\Users\joshu\Pictures\Screenshots\Screenshot 2024-03-02 085154.png"
+# },
+# "TITLE_INFO": {
+# 	"region_name": SearchAreaLocation.TITLE_INFO,
+# 	"image_path": r"C:\Users\joshu\Pictures\Screenshots\Screenshot 2024-03-02 085603.png"
+# },
+# "ORIGIN_POS": {
+# 	"region_name": SearchAreaLocation.ORIGIN_POS,
+# 	"image_path": r"C:\Users\joshu\Pictures\Screenshots\Screenshot 2024-03-03 014408.png"
+# },
+# "DEST_POS": {
+# 	"region_name": SearchAreaLocation.DEST_POS,
+# 	"image_path": r"C:\Users\joshu\Pictures\Screenshots\Screenshot 2024-03-03 020140.png"
+# },
+# 	"ORBITAL_HUD_LANDED": {
+# 		"region_name": SearchAreaLocation.ORBITAL_HUD_LANDED,
+# 		"image_path": r"C:\Repositories\Dual Universe\Missions Dual Universe\data\search_areas\ORBITAL_HUD_LANDED.png"
+# 	},
+# }
+#
+# for key, value in region_dict.items():
+# 	region_name = value["region_name"]
+# 	image_path = value["image_path"]
+#
+# 	obj.get_image_bbox(region_name=region_name,
+# 	                   image_path=image_path)
--- models/sqlalchemy_db.py
+++ models/sqlalchemy_db.py
@@ -1,13 +1,143 @@
+import json
 import os
+import uuid
+from datetime import datetime
 
-from sqlalchemy import create_engine
+import sqlalchemy
+from sqlalchemy.orm import load_only
+
+from logs.logging_config import logger
+from sqlalchemy import create_engine, text, insert
+from sqlmodel import SQLModel, Session, select
+
 from config.config_manager import ConfigManager
+from models import SearchArea, ImageLocation, Image, Mission
 from path_router import DirectoryPaths
 
 
-def connect_to_db():
-	# Database URL
+class DbConfig:
 	config_manager = ConfigManager()
+	memory_engine = create_engine(f"sqlite:///:memory:")
+
 	engine = create_engine(
-		f"sqlite:///{os.path.join(DirectoryPaths.MODEL_DIR, config_manager.get_value('config.database'))}", echo=True)
-	return engine
+		f"sqlite:///{os.path.join(DirectoryPaths.ROOT_DIR, config_manager.get_value('config.database'))}",
+		echo=False,
+	)
+
+	def __init__(self, images_dir=None):
+		self.config_manager = ConfigManager()
+		self.images_dir = images_dir or os.path.relpath(DirectoryPaths.DU_IMAGES_DIR)
+
+	def dump_table_Mission(self):
+		try:
+			with Session(self.engine) as session:
+				query = session.exec(select(Mission)).all()
+				excluded_fields = {'updated_at', 'created_at', 'id'}
+
+			# Extract only the required fields for SearchArea
+			filtered_data = []
+			for data in query:
+				filtered_data.append(
+					{
+						field_name: getattr(data, field_name)
+						for field_name in data.model_dump().keys()
+						if field_name not in excluded_fields
+					}
+				)
+
+			# Dump the data to a JSON file
+			file_path = "../config/db_table/Mission_table.json"
+			with open(file_path, "w+") as json_file:
+				json.dump(filtered_data, json_file, indent=4)
+		except Exception as e:
+			logger.error(f"Error dumping table Mission_table: {e}")
+
+	def dump_table_Image(self):
+		try:
+			with Session(self.engine) as session:
+				query = session.exec(select(Image)).all()
+				excluded_fields = {'updated_at', 'created_at', 'id'}
+
+			# Extract only the required fields for SearchArea
+			filtered_data = []
+			for data in query:
+				filtered_data.append(
+					{
+						field_name: getattr(data, field_name)
+						for field_name in data.model_dump().keys()
+						if field_name not in excluded_fields
+					}
+				)
+
+			# Dump the data to a JSON file
+			file_path = "../config/db_table/Image_table.json"
+			with open(file_path, "w+") as json_file:
+				json.dump(filtered_data, json_file, indent=4)
+		except Exception as e:
+			logger.error(f"Error dumping table Image_table: {e}")
+
+	def dump_table_SearchArea(self):
+		try:
+			with Session(self.engine) as session:
+				query = session.exec(select(SearchArea)).all()
+				excluded_fields = {'updated_at', 'created_at', 'id'}
+
+			# Extract only the required fields for SearchArea
+			filtered_data = []
+			for data in query:
+				filtered_data.append(
+					{
+						field_name: getattr(data, field_name)
+						for field_name in data.model_dump().keys()
+						if field_name not in excluded_fields
+					}
+				)
+
+			# Dump the data to a JSON file
+			file_path = "../config/db_table/SearchArea_table.json"
+			with open(file_path, "w+") as json_file:
+				json.dump(filtered_data, json_file, indent=4)
+		except Exception as e:
+			logger.error(f"Error dumping table SearchArea_table: {e}")
+
+	def create_db_and_tables(self):
+		SQLModel.metadata.create_all(self.engine)
+
+	def load_SearchArea_table(self):
+		with Session(self.engine) as session:
+			pass
+
+	def load_Image_table(self):
+		try:
+			# Load the JSON data from the file
+			file_path = "../config/db_table/Image_table.json"
+			with open(file_path, "r") as json_file:
+				json_data = json.load(json_file)
+
+			with Session(self.memory_engine) as session:
+				for item in json_data:
+					# item_dict = item.dict()
+					# Create a new Image instance with the data from the JSON
+					image = Image(**item)
+					image.id = uuid.uuid4()
+					# image.updated_at = datetime.now()
+					image.created_at = datetime.now()
+					# Insert the Image instance into the table
+					session.add(image)
+
+				# Commit the transaction
+				session.commit()
+		except Exception as e:
+			logger.error(f"Error loading table Image_table.json: {e}")
+
+	def load_Mission_table(self):
+		with Session(self.engine) as session:
+			pass
+
+
+if __name__ == "__main__":
+	config = DbConfig()
+	config.dump_table_SearchArea()
+	config.dump_table_Image()
+	config.dump_table_Mission()
+# config.load_Image_table()
